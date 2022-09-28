611c44a2d22065edcdf801fc2e54b74745a60a8a
Will <author@will.party>
2022-09-28 02:17:45 -0700

    Clean up macro detection

--- test/integration/basic/input.scm
+++ test/integration/basic/input.scm
@@ -1,9 +1,14 @@
 
 ;; A dummy source file for experimentation.
 
+(define-syntax derspler
+  (syntax-rules ()
+    ((derspler obj port ...)
+     (display obj port ...))))
+
 (define x "Does anybody care?")
 
-(display x)
+(derspler x)
 
 
 (define y #f)
