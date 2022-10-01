b372ee9c56ec745508ff5725e1e97294cc1ede2b
Will <author@will.party>
2022-09-30 17:46:33 -0700

    Basic user macro definition

--- test/integration/basic/input.scm
+++ test/integration/basic/input.scm
@@ -2,7 +2,7 @@
 ;; A dummy source file for experimentation.
 
 (define-syntax derspler
-  (syntax-rules ()
+  (syntax-rules (hey there sher)
     ((derspler obj port ...)
      (display obj port ...))))
 
